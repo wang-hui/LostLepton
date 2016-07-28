d297475252ac35e213a68239dfd2e167c6414db9
Florent <lacroix@cern.ch>
2016-07-28 18:00:50 +0200

    app ICHEP

--- Tool/CMS_lumi.h
+++ Tool/CMS_lumi.h
@@ -28,7 +28,9 @@ namespace CMSStylePlot
   float extraOverCmsTextSize  = 0.76;
 
   //TString lumi_13TeV = "2.3 fb^{-1}";
-  TString lumi_13TeV = "4.0 fb^{-1}";
+  //TString lumi_13TeV = "4.0 fb^{-1}";
+  //TString lumi_13TeV = "7.6 fb^{-1}";
+  TString lumi_13TeV = "12.9 fb^{-1}";
   TString lumi_8TeV  = "19.7 fb^{-1}";
   TString lumi_7TeV  = "5.1 fb^{-1}";
   TString lumi_sqrtS = "(13 TeV)";
